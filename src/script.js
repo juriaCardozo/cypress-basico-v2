be4c8d077f97c1ae0b6f2da082f754f9ec324343
julia.cavalcante <ccardozojulia@gmail.com>
2024-01-23 15:43:50 -0300

    Break the app on purpose

--- src/script.js
+++ src/script.js
@@ -3,7 +3,7 @@ let isPhoneRequired = false
 const phoneLabelSpan = document.querySelector('.phone-label-span')
 const phoneField = document.getElementById('phone')
 
-document.querySelector('#phone-checkbox')
+document.quer('#phone-checkbox')
   .addEventListener('change', function() {
     if (this.checked) {
       phoneLabelSpan.style.display = 'inline'
